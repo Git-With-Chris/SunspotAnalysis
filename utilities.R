b2cf03f31a616e062e79de1a3a85df742b66e5e4
Chris John <132337461+Git-With-Chris@users.noreply.github.com>
2024-06-15 18:35:22 +1000

    Updated notebook - Appendix Pending

--- utilities.R
+++ utilities.R
@@ -17,7 +17,7 @@ sort.score <- function(x, score = c("bic", "aic")){
 # Fitted ARIMA Models Summary generator
 # Example: arima_summary(Data, c(1,1,1))
 
-arima_summary <- function(TS_data, pdq_order){
+arima_summary <- function(TS_data, pdq_order, seasonal_order, period){
 
   # Check if TS_data is of class 'ts'
   if (!inherits(TS_data, "ts")) {
@@ -25,29 +25,35 @@ arima_summary <- function(TS_data, pdq_order){
   }
 
   # Estimate ARIMA model using CSS method
-  model_css <- TS_data %>% Arima(order = pdq_order, method = 'CSS')
+  model_css <- TS_data %>% Arima(order = pdq_order,
+                                 seasonal = list(order=seasonal_order, period = period),
+                                 method = 'CSS')
 
   # Print parameter estimation results for CSS method
   cat("\nParameter Estimation through Least Squares (CSS) Method for ARIMA(",
-      pdq_order, ")\n")
+      pdq_order, ")(1,1,2)[11]\n")
   print(lmtest::coeftest(model_css))
   cat("==============================================================")
 
   # Estimate ARIMA model using ML method
-  model_ml <- TS_data %>% Arima(order = pdq_order, method = 'ML')
+  model_ml <- TS_data %>% Arima(order = pdq_order,
+                                seasonal = list(order=seasonal_order, period = period),
+                                method = 'ML')
 
   # Print parameter estimation results for ML method
   cat("\nParameter Estimation through Maximum Likelihood (ML) Method for ARIMA(",
-      pdq_order, ")\n")
+      pdq_order, ")(1,1,2)[11]\n")
   print(lmtest::coeftest(model_ml))
   cat("==============================================================")
 
   # Estimate ARIMA model using CSS-ML method
-  model_cssml <- TS_data %>% Arima(order = pdq_order, method = 'CSS-ML')
+  model_cssml <- TS_data %>% Arima(order = pdq_order,
+                                   seasonal = list(order=seasonal_order, period = period),
+                                   method = 'CSS-ML')
 
   # Print parameter estimation results for CSS-ML method
   cat("\nParameter Estimation through Combination (CSS-ML) Method for ARIMA(",
-      pdq_order, ")\n")
+      pdq_order, ")(1,1,2)[11]\n")
   print(lmtest::coeftest(model_cssml))
   cat("==============================================================")
 
@@ -92,25 +98,37 @@ check_residuals <- function(TS_data, order, seasonal_order, period,
                             res_main='Residuals',
                             hist_main='Histogram',
                             acf_main='ACF',
-                            pacf_main='PACF'){
-  # Set up the plotting area to have 2 plots side by side
-  par(mfrow = c(2, 2))
+                            pacf_main='PACF',
+                            qq_main='QQ-Plot'){
 
+  # Set up the plotting area to have 2 plots side by side
+  par(mfrow = c(2, 3))
+
+  library(FitAR)
   # Obtain Residuals
   residuals <- TS_data %>%
     Arima(order = order,
           seasonal = list(order=seasonal_order, period = period)) %>%
     rstandard()
 
+  print(shapiro.test(residuals))
+
   # Plot Residuals
   residuals %>% plot(type = 'o',
                      main = res_main,
-                     ylab = 'Residuals')
+                     ylab = 'Standardized Residuals',
+                     lwd=1)
+  abline(h=0, lty=2, col='blue', lwd=2)
 
   grid(nx = NULL, ny = NULL, col = "gray", lty = "dotted")
 
   # Plot Histogram
   residuals %>% hist(main = hist_main)
+  grid(nx = NULL, ny = NULL, col = "gray", lty = "dotted")
+
+  # Plot QQ-Plot
+  residuals %>% qqnorm(main = qq_main)
+  residuals %>% qqline(lty=2, col='blue')
   grid(nx = NULL, ny = NULL, col = "gray", lty = "dotted")
 
   # Plot ACF
@@ -120,6 +138,17 @@ check_residuals <- function(TS_data, order, seasonal_order, period,
   # Plot PACF
   residuals %>% pacf(main=pacf_main, lag.max = pacf_lag)
   grid(nx = NULL, ny = NULL, col = "gray", lty = "dotted")
+
+  # LJung-Box Test
+  k=0
+  if (length(residuals) < 30){
+    lagM <- length(residuals) - 1
+  } else {
+    lagM <- 29
+  }
+  LBQPlot(residuals, lag.max = lagM, StartLag = k + 1, k = 0, SquaredQ = FALSE)
+  grid(nx = NULL, ny = NULL, col = "gray", lty = "dotted")
+
 
   # Reset plotting area to default
   par(mfrow = c(1, 1))
